fc1882038caad315578b85c8c52cc9817f173ee3
Samuel Sleight <samuel.sleight@gmail.com>
2014-01-26 15:40:26 +0000

    Cleaned up unused funcs

--- Exercise 1/src/part3/Part3.java
+++ Exercise 1/src/part3/Part3.java
@@ -1,29 +1,22 @@
 package part3;
 
-import java.util.ArrayList;
-import java.util.List;
 import lejos.nxt.Button;
 import lejos.nxt.ButtonListener;
 import lejos.nxt.LCD;
 import lejos.nxt.SensorPort;
 import lejos.nxt.TouchSensor;
-import lejos.nxt.UltrasonicSensor;
 import lejos.robotics.navigation.DifferentialPilot;
 import lejos.util.Delay;
 import util.LCDUtil;
 import util.RobotInfo;
 
 public class Part3 implements Runnable {
-	private static final int WALL_HUG_DIST = 5;
-
 	private DifferentialPilot pilot;
-	private UltrasonicSensor leftSensor;
 	private TouchSensor bumperA, bumperB;
 
-	public Part3(DifferentialPilot pilot, UltrasonicSensor leftSensor,
+	public Part3(DifferentialPilot pilot,
 			TouchSensor bumperA, TouchSensor bumperB) {
 		this.pilot = pilot;
-		this.leftSensor = leftSensor;
 		this.bumperA = bumperA;
 		this.bumperB = bumperB;
 	}
@@ -62,48 +55,6 @@ public class Part3 implements Runnable {
 		pilot.forward();
 	}
 
-	private int removeAnomaliesAndGetAverage(List<Integer> list) {
-		int sum = 0;
-		for (int i = 0; i < list.size(); i++) {
-			sum += list.get(i);
-		}
-
-		int mean = sum / list.size();
-		int meandiffs = 0;
-
-		for (int i = 0; i < list.size(); i++) {
-			meandiffs += ((list.get(i) - mean) * (list.get(i) - mean));
-		}
-
-		double sigma = Math.sqrt(meandiffs / list.size());
-
-		boolean changed = false;
-		for (int i = 0; i < list.size(); i++) {
-			if (Math.abs(list.get(i) - mean) > sigma) {
-				list.remove(i);
-				i--;
-				changed = true;
-			}
-		}
-
-		if (changed) {
-			return removeAnomaliesAndGetAverage(list);
-		} else {
-			return mean;
-		}
-
-	}
-
-	private int getSensorDistance(UltrasonicSensor sensor, int numChecks,
-			int timeInterval) {
-		List<Integer> vals = new ArrayList<Integer>();
-		for (int i = 0; i < numChecks; i++) {
-			vals.add(sensor.getDistance());
-			Delay.msDelay(timeInterval);
-		}
-		return removeAnomaliesAndGetAverage(vals);
-	}
-
 	private boolean isBumperPressed() {
 		return bumperA.isPressed() || bumperB.isPressed();
 	}
@@ -117,8 +68,7 @@ public class Part3 implements Runnable {
 		LCD.clear();
 
 		Part3 p = new Part3(RobotInfo.SEBASTIAN.getDifferentialPilot(),
-				new UltrasonicSensor(SensorPort.S3), new TouchSensor(
-						SensorPort.S1), new TouchSensor(SensorPort.S4));
+				new TouchSensor(SensorPort.S1), new TouchSensor(SensorPort.S4));
 		p.run();
 	}
 
