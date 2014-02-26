eb808fba6bfc1b1bb9c2d74a2c1ab1f6dfb05e43
Bertie Wheen <wheen.b@gmail.com>
2014-02-26 19:52:45 +0000

    It is with great sadness that I delete seb3d

--- seb3d/seb3d/rasterizer/Face.java
+++
@@ -1,9 +0,0 @@
-package rasterizer;
-
-public class Face {
-    public int[] points;
-
-    public Face(int[] points) {
-        this.points = points;
-    }
-}
--- seb3d/seb3d/rasterizer/Main.java
+++
@@ -1,8 +0,0 @@
-package rasterizer;
-
-
-public class Main {
-	public static void main(String[] args) {
-		Demos.rotatingCube();
-	}
-}
