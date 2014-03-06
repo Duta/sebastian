46df7c3fcc895ecbd42ce0e16e5302a2f2057a8d
Bertie Wheen <wheen.b@gmail.com>
2014-03-06 10:35:41 +0000

    ArrayUtil::swap for some primitives

--- RobotUtils/src/util/ArrayUtil.java
+++ RobotUtils/src/util/ArrayUtil.java
@@ -25,4 +25,16 @@ public class ArrayUtil {
         array[i] = array[j];
         array[j] = temp;
     }
+
+    public static void swap(char[] array, int i, int j) {
+        char temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
