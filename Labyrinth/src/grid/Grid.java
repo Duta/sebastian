f24393aa1f6a8aeaa61b58ed7e831faa90a8b26f
Duta <wheen.b@gmail.com>
2014-03-18 15:42:43 +0000

    Grid(GridMap) constructor

--- Labyrinth/src/grid/Grid.java
+++ Labyrinth/src/grid/Grid.java
@@ -1,14 +1,16 @@
 package grid;
 
+import rp.robotics.mapping.GridMap;
 import util.Util;
-import lejos.robotics.mapping.LineMap;
+import lejos.geom.Line;
+import lejos.geom.Rectangle;
 
 public class Grid {
     private final boolean[][][] grid;
     private final int width;
     private final int height;
     
-    public Grid(int width, int height) {
+    private Grid(int width, int height) {
         this.width = width;
         this.height = height;
         this.grid = new boolean[width][height][4];
@@ -19,24 +21,31 @@ public class Grid {
             this.grid[x][y][i] = true;
         }
     }
-    
-    public Grid(LineMap lineMap) {
-        this(Util.round(lineMap.getBoundingRect().getWidth()),
-             Util.round(lineMap.getBoundingRect().getHeight()));
-        // TODO: add blockages
+
+    public Grid(GridMap gridMap) {
+        this(gridMap.getGridWidth(),
+             gridMap.getGridHeight());
+        for(int x = 0; x < width; x++)
+        for(int y = 0; y < height; y++)
+        for(GridDirection dir : GridDirection.values()) {
+            float range = gridMap.rangeToObstacleFromGridPoint(x, y, dir.degrees);
+            if(range < gridMap.getCellSize() * 0.8f) {
+                setBlocked(x, y, dir);
+            }
+        }
+    }
+
+    public void setBlocked(int x, int y, GridDirection dir) {
+        grid[x][y][dir.index] = false;
+        grid[x + dir.dx][y + dir.dy][dir.opposite] = false;
     }
     
-    public void setBlocked(int x, int y, GridDirection direction) {
-        grid[x][y][direction.index] = false;
-        grid[x + direction.dx][y + direction.dy][direction.opposite] = false;
-    }
-    
-    public boolean canGo(int x, int y, GridDirection direction) {
+    public boolean canGo(int x, int y, GridDirection dir) {
         if(x < 0 || x >= width
         || y < 0 || y >= height) {
             return false;
         }
-        return grid[x][y][direction.index];
+        return grid[x][y][dir.index];
     }
 
     public int getWidth() {
--- Labyrinth/src/grid/GridDirection.java
+++ Labyrinth/src/grid/GridDirection.java
@@ -1,18 +1,20 @@
 package grid;
 
 public enum GridDirection {
-    UP(0, 0, -1, 1),
-    DOWN(1, 0, 1, 0),
-    LEFT(2, -1, 0, 3),
-    RIGHT(3, 1, 0, 2);
+    UP(0, 0, -1, 1, 0f),
+    DOWN(1, 0, 1, 0, 180f),
+    LEFT(2, -1, 0, 3, 270f),
+    RIGHT(3, 1, 0, 2, 90f); // TODO: Check LEFT and RIGHT degrees values
     
     public final int index, opposite; 
     public final int dx, dy;
+    public final float degrees;
     
-    private GridDirection(int index, int dx, int dy, int opposite) {
+    private GridDirection(int index, int dx, int dy, int opposite, float degrees) {
         this.index = index;
         this.dx = dx;
         this.dy = dy;
         this.opposite = opposite;
+        this.degrees = degrees;
     }
 }
